a250e56514a3afb53d87a5cab6187bca57ec2a66
PavelZubkov <zubkov_p@mail.ru>
2021-07-31 14:01:35 +0300

    Init

---
+++ timer.web.ts
@@ -0,0 +1,47 @@
+namespace $ {
+
+	export class $my_timer extends $mol_object2 {
+		timer_id: any
+		
+		@ $mol_mem
+		time( next? : Date ) {
+			return next ?? new Date
+		}
+		
+		@ $mol_mem
+		period( ms? : number ) {
+			return ms ?? 100
+		}
+		
+		start() {
+			this.loop()
+		}
+		
+		stop() {
+			clearTimeout( this.timer_id )
+		}
+		
+		loop() {
+			this.time( new Date() )
+			this.timer_id = setTimeout( () => this.loop() , this.period() )
+		}
+	}
+	
+	export class $my_timer_app extends $mol_object2 {
+		@ $mol_mem
+		static timer() {
+			return new $my_timer
+		}
+
+		@ $mol_mem
+		static autorun() {
+			const timer = this.timer()
+			return $mol_atom2_autorun( ()=> {
+				console.log('RENDER')
+				return document.getElementById('root')!.innerHTML = timer.time().toISOString()
+			} )
+		}
+	}
+	
+	$my_timer_app.autorun()
+}
